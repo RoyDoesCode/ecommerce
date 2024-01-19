acc0da347c0cbce0688e60d98d6dcc55bdcd757d
Roy Barzilay <roybar03@gmail.com>
2024-01-19 16:35:37 +0200

    Add store switcher component

--- admin/components/navbar.tsx
+++ admin/components/navbar.tsx
@@ -1,13 +1,28 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
 import MainNav from "@/components/mainNav";
+import StoreSwitcher from "@/components/storeSwitcher";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect("/sign-in");
+    }
+
+    const stores = await prismadb.store.findMany({
+        where: {
+            userId,
+        },
+    });
+
     return (
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
-                <div>This will be a store switcher</div>
+                <StoreSwitcher items={stores} />
 
                 <MainNav className="mx-6" />
 
---
+++ admin/components/storeSwitcher.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import React, { useMemo, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { Store } from "@prisma/client";
+import {
+    Check,
+    ChevronsUpDown,
+    PlusCircle,
+    Store as StoreIcon,
+} from "lucide-react";
+
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+} from "@/components/ui/command";
+import useStoreModalStore from "@/hooks/useStoreModalStore";
+import { cn } from "@/lib/utils";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<
+    typeof PopoverTrigger
+>;
+
+interface StoreSwitcherProps extends PopoverTriggerProps {
+    items?: Store[];
+}
+
+const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
+    const [open, setOpen] = useState(false);
+
+    const storeModalStore = useStoreModalStore();
+    const params = useParams();
+    const router = useRouter();
+
+    const currentStore = useMemo(
+        () => items.find((item) => item.id === params.storeId),
+        [items]
+    );
+
+    const onStoreSelect = (store: Store) => {
+        setOpen(false);
+        router.push(`/${store.id}`);
+    };
+
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    role="combobox"
+                    aria-expanded={open}
+                    aria-label="Select a store"
+                    className={cn("w-[200px] justify-between", className)}
+                >
+                    <StoreIcon className="mr-2 h-4 w-4" />
+                    {currentStore?.name}
+                    <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+            </PopoverTrigger>
+
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandList>
+                        <CommandInput placeholder="Search store..." />
+                        <CommandEmpty>No store found</CommandEmpty>
+                        <CommandGroup heading="Stores">
+                            {items.map((store) => (
+                                <CommandItem
+                                    key={store.id}
+                                    onSelect={() => onStoreSelect(store)}
+                                    className="text-sm"
+                                >
+                                    <StoreIcon className="mr-2 h-4 w-4" />
+                                    {store.name}
+                                    <Check
+                                        className={cn(
+                                            "ml-auto h-4 w-4",
+                                            currentStore?.id === store.id
+                                                ? "opacity-100"
+                                                : "opacity-0"
+                                        )}
+                                    />
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                    </CommandList>
+                    <CommandSeparator />
+                    <CommandList>
+                        <CommandGroup>
+                            <CommandItem
+                                onSelect={() => {
+                                    setOpen(false);
+                                    storeModalStore.onOpen();
+                                }}
+                                className="cursor-pointer"
+                            >
+                                <PlusCircle className="mr-2 h-5 w-5" />
+                                Create Store
+                            </CommandItem>
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+        </Popover>
+    );
+};
+
+export default StoreSwitcher;
