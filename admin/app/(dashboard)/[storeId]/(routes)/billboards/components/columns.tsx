2da6aab8be9f4652ce492a3feae270498f96c37c
Roy Barzilay <roybar03@gmail.com>
2024-01-26 11:05:15 +0200

    Add billboard image to billboard grid

--- admin/app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -3,10 +3,12 @@
 import { ColumnDef } from "@tanstack/react-table";
 
 import CellAction from "./cellAction";
+import Image from "next/image";
 
 export type BillboardColumn = {
     id: string;
     label: string;
+    imageUrl: string;
     createdAt: string;
 };
 
@@ -14,6 +16,29 @@ export const columns: ColumnDef<BillboardColumn>[] = [
     {
         accessorKey: "label",
         header: "Label",
+    },
+    {
+        accessorKey: "imageUrl",
+        header: "Image",
+        cell: ({ row }) => (
+            <div
+                className="
+                    relative 
+                    aspect-square 
+                    h-10
+                    w-10
+                    overflow-hidden 
+                    rounded-sm"
+            >
+                <Image
+                    fill
+                    sizes="100%"
+                    src={row.original.imageUrl}
+                    alt="Billboard Image"
+                    className="object-cover object-center"
+                />
+            </div>
+        ),
     },
     {
         accessorKey: "createdAt",
