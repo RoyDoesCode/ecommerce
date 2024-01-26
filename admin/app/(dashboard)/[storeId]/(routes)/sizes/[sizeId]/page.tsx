624679e6b6a635752f3ec3a7b42f49bad0182a28
Roy Barzilay <roybar03@gmail.com>
2024-01-26 19:26:26 +0200

    Fix import errors

--- admin/app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page.tsx
@@ -1,5 +1,6 @@
 import prismadb from "@/lib/prismadb";
-import SizeForm from "./components/sizeForm";
+
+import SizeForm from "./components/sizesForm";
 
 const SizePage = async ({ params }: { params: { sizeId: string } }) => {
     const size = await prismadb.size.findUnique({
